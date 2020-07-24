60cdc38f70a06e7ff02fd6a6d28afda2b9f5b1c1
Joiney Nguyen <54327713+joineynguyen@users.noreply.github.com>
2020-07-23 19:23:44 -0500

    Update RemoveDups.java

--- Linked List/RemoveDups.java
+++ Linked List/RemoveDups.java
@@ -1,123 +1,60 @@
 //By: Joiney Nguyen
 //Description: Remove duplicates in the linked list.
+//UPDATE (7/23/2020) Changed LinkedList class to ListNode class for cleaner code. Also added a second solution that uses no buffer.
+ public void removeDups(ListNode n) 
+    {
+        if(n == null)
+        {
+            return;
+        }
 
-import java.util.*;
+        HashSet<Integer> myHash = HashSet<Integer>();
 
-public class LinkedList
+        ListNode prevNode = null;
+   
+        while(n != null)
+        {
+            if(myHash.contains(n.data))
+            {
+                prevNode.next = n.next;
+            }
+            else
+            {
+                myHash.add(n.data);
+                prevNode = n;
+            }
+
+            
+            n = n.next;
+        }
+    }
+
+
+public void removeDups(ListNode head) //no buffer
 {
-
-	Node head;
-
-	static class Node
+	if(head == null)
 	{
-		int data;
-		Node next;
-
-		Node(int d)
-		{
-			data = d;
-		}
+		return;
 	}
 
+	ListNode currentNode = head;
 
-	public static void append(LinkedList list, int data)
+	while(currentNode != null)
 	{
-		Node new_node = new Node(data);
-		
+		ListNode scannerNode = currentNode;
 
-		if(list.head == null)
+		while(scannerNode.next != null)
 		{
-			list.head = new_node;
-			new_node.next = null;
-		}
-		else
-		{
-			Node last = list.head;
-
-			while(last.next != null)
+			if(scanner.next.data == currentNode.data)
 			{
-				last = last.next;
-			}
-
-			last.next = new_node;
-			new_node.next = null;
-			
-		}
-
-	}
-
-	public static void deleteDuplicate(LinkedList list)
-	{
-		HashSet<Integer> set = new HashSet<Integer>();
-
-		if(list.head == null)
-		{
-			return;
-		}
-		else
-		{
-			Node currentNode = list.head;
-			Node prevNode = null;
-			
-			while(currentNode != null)
-			{
-				int currentInt = currentNode.data;	
-				
-				if(set.contains(currentInt) == true)
-				{
-					prevNode.next = currentNode.next;
-					currentNode = currentNode.next;
-				}
-				else
-				{
-					set.add(currentInt);
-					prevNode = currentNode;
-					currentNode = currentNode.next;
-				}
-
-			}
-		}
-	}
-
-		public static void print(LinkedList list)
-		{
-			if(list.head == null)
-			{
-				return;
+				scanner.next = scanner.next.next;
 			}
 			else
 			{
-				Node currentNode = list.head;
-
-				while(currentNode != null)
-				{
-					System.out.println(currentNode.data);
-					
-					currentNode = currentNode.next;
-				}
+				scanner = scanner.next;
 			}
+
+			currentNode = currentNode.next;
 		}
-	
-
-
-
-	public static void main(String[] args)
-	{
-		LinkedList list = new LinkedList();
-
-		append(list, 1);
-		append(list, 1);
-		append(list, 2);
-		append(list, 3);
-		append(list, 4);
-		append(list, 5);
-		append(list, 5);
-	    
-	    print(list);
-	    
-	    deleteDuplicate(list);
-	    
-	    System.out.println();
-	    print(list);
 	}
 }
