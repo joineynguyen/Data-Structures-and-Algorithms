5bbe69eed9caa061238ca978915aababea4a7a5b
Joiney Nguyen <54327713+joineynguyen@users.noreply.github.com>
2020-08-05 03:14:09 -0500

    Update RouteBetweenNodes.java

--- TreesAndGraphs/RouteBetweenNodes.java
+++ TreesAndGraphs/RouteBetweenNodes.java
@@ -88,20 +88,20 @@ public class Graph
 	{
 		Graph g = new Graph(4);
 		g.addEdge(0, 1); 
-        g.addEdge(0, 2); 
-        g.addEdge(1, 2); 
-        g.addEdge(2, 0); 
-        g.addEdge(2, 3); 
-        g.addEdge(3, 3); 
+        	g.addEdge(0, 2); 
+        	g.addEdge(1, 2); 
+       		g.addEdge(2, 0); 
+        	g.addEdge(2, 3); 
+        	g.addEdge(3, 3); 
 
-        if(g.isReachable(1,3))
-        {
-        	System.out.println("True");
-        }
-        else
-        {
-        	System.out.println("False");
-        }
+		if(g.isReachable(1,3))
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
   
 	}
 
