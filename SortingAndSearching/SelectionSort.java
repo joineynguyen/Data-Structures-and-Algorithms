fd213fb7442d12ecc78b0527009e83086d00f8f3
Joiney Nguyen <54327713+joineynguyen@users.noreply.github.com>
2020-09-07 10:03:58 -0500

    Update SelectionSort.java

--- SortingAndSearching/SelectionSort.java
+++ SortingAndSearching/SelectionSort.java
@@ -3,39 +3,47 @@
  * 
  * Selection sort on unsorted int array. The smallest element is selected from the unsorted array and swapped with the leftmost element, and that element becomes a part of the sorted array. This process continues moving unsorted array boundary from one element to the right.
  */
-public class Prac 
+public class Prac2 
 {
-	static void selectionSort(int[] arr)
-	{
-		int arrSize = arr.length;
-		int temp;
-		
-		for(int i = 0; i < arrSize - 1; i++)
-		{
-			for(int j = i + 1; j < arrSize - 1; j++)
-			{
-				if(arr[j] < arr[i])
-				{
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-	
-		}
-	}
+    static void selectionSort(int[] arr)
+    {
+        int arrSize = arr.length;
+        int temp;
+               
+        for(int i = 0; i < arrSize - 1; i++)
+        {
+            int currentMinIndex = i;
+            
+            for(int j = i + 1; j < arrSize; j++)
+            {
+                if(arr[j] < arr[currentMinIndex])
+                {
+                    currentMinIndex = j;
+                }
+            }
+           
+            if(currentMinIndex != i)
+            {
+                temp = arr[i];
+                arr[i] = arr[currentMinIndex];
+                arr[currentMinIndex] = temp;
+            }
+            
+            
+        }
+    }
 	
 	static void printArray(int[] arr)
 	{
 		for(int i = 0; i < arr.length - 1; i ++)
 		{
-			System.out.println(arr[i]);
+			System.out.print(arr[i]+ " ");
 		}
 	}
 	
 	public static void main(String[] args)
 	{
-		int[] arr = {9, 8, 7, 6, 3, 1, 2};
+		int[] arr = {9, 8, 7, 6, 3, 1, 2, 2};
 		selectionSort(arr);
 		printArray(arr);
 		
