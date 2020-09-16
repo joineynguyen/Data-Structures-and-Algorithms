dc40fe8cf20d26bdb2f72e0ba745b6558a6102d7
Joiney Nguyen <54327713+joineynguyen@users.noreply.github.com>
2020-09-16 17:25:48 -0500

    Create KClosestPointsToOrigin.java

---
+++ Heaps/KClosestPointsToOrigin.java
@@ -0,0 +1,83 @@
+/* 
+Joiney Nguyen
+
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+ 
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+*/
+
+
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.lang.Math;
+
+class Solution 
+{
+    public int[][] kClosest(int[][] points, int K) 
+    {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>()
+                                                           {
+                                                                public int compare(int[] point1, int[] point2) 
+                                                                {
+                                                                    double num1 = getHypotenuse(point1);
+                                                                    double num2 = getHypotenuse(point2);                                                                   
+                                                                    if (num1 < num2) 
+                                                                    {
+                                                                        return 1;
+                                                                    }
+                                                                    else if (num1 > num2) 
+                                                                    {
+                                                                        return -1;
+                                                                    }
+                                                                    else 
+                                                                    {
+                                                                        return 0;
+                                                                    }
+                                                                }
+
+                                                                public double getHypotenuse(int[] point)
+                                                                {
+                                                                    double a = point[0];
+                                                                    double b = point[1];
+
+                                                                    a = Math.pow(a, 2);
+                                                                    b = Math.pow(b, 2);
+
+                                                                    return Math.sqrt(a + b);
+                                                                }
+                                                           });
+        
+        for(int[] point : points)
+        {
+            pq.offer(point);
+            
+            if(pq.size() > K)
+            {
+                pq.poll();
+            }
+        }
+        
+        int[][] result = new int[K][2];
+        
+        for(int i = 0; i < K; i++)
+        {
+            result[i] = pq.poll();
+        }
+        
+        return result;
+    }
+}
