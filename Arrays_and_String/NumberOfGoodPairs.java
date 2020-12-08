e0b20f2be57ce3c056397f4131e1b4e9c317dcf4
Joiney Nguyen <54327713+joineynguyen@users.noreply.github.com>
2020-12-08 07:07:15 -0600

    Create NumberOfGoodPairs.java

---
+++ Arrays_and_String/NumberOfGoodPairs.java
@@ -0,0 +1,31 @@
+/*
+Joiney Nguyen
+
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+*/
+
+class Solution 
+{
+    public int numIdenticalPairs(int[] nums) 
+    {
+        int count = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = i + 1; j < nums.length; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
